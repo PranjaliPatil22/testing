4bf2b33bdc222e7e85c3769ed2f4357ec9c4aefd
shruti-kasture109 <shrutikasture109@gmail.com>
2025-04-26 17:20:38 +0530

    uu

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const path = require('path'); // <-- Important
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // MySQL connection configuration
 // const db = mysql.createConnection({
